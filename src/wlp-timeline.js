369cc3339bed2c39fedc74e00b908a7507ee6765
Jamie Williams <jamieinfinity@mac.com>
2017-05-07 12:34:41 -0500

    stripped it down to core zoomable timeline; can now start building back up toward previous version, but using v4 api - Jamie

--- src/wlp-timeline.js
+++ src/wlp-timeline.js
@@ -5,48 +5,38 @@ function makeTimeline() {
     // Using mbostock's block as a starting point for migrating timeline to v4:
     // https://bl.ocks.org/mbostock/34f08d5e11952a80609169b7917d4172
 
-    var svg = d3.select("svg"),
-        margin = {top: 20, right: 20, bottom: 110, left: 40},
-        margin2 = {top: 430, right: 20, bottom: 30, left: 40},
-        width = +svg.attr("width") - margin.left - margin.right,
-        height = +svg.attr("height") - margin.top - margin.bottom,
-        height2 = +svg.attr("height") - margin2.top - margin2.bottom;
+    const timelineMargin = {top: 20, right: 20, bottom: 30, left: 20};
+    const timelineSize = {
+        height: 70 - timelineMargin.top - timelineMargin.bottom,
+        width: 900 - timelineMargin.top - timelineMargin.bottom
+    };
 
-    var parseDate = d3.timeParse("%b %Y");
+    const svg = d3.select("svg");
 
-    var x = d3.scaleTime().range([0, width]),
-        x2 = d3.scaleTime().range([0, width]),
-        y = d3.scaleLinear().range([height, 0]),
-        y2 = d3.scaleLinear().range([height2, 0]);
+    const parseDate = d3.timeParse("%b %Y");
 
-    var xAxis = d3.axisBottom(x),
-        yAxis = d3.axisLeft(y);
+    const x = d3.scaleTime().range([0, timelineSize.width]),
+        x0 = d3.scaleTime().range([0, timelineSize.width]);
 
-    var zoom = d3.zoom()
+    const xAxis = d3.axisBottom(x)
+        .tickSize(-timelineSize.height)
+        .tickPadding(6);
+
+    const zoom = d3.zoom()
         .scaleExtent([1, Infinity])
-        .translateExtent([[0, 0], [width, height]])
-        .extent([[0, 0], [width, height]])
+        .translateExtent([[0, 0], [timelineSize.width, timelineSize.height]])
+        .extent([[0, 0], [timelineSize.width, timelineSize.height]])
         .on("zoom", zoomed);
-
-    var area = d3.area()
-        .curve(d3.curveMonotoneX)
-        .x(function (d) {
-            return x(d.date);
-        })
-        .y0(height)
-        .y1(function (d) {
-            return y(d.price);
-        });
 
     svg.append("defs").append("clipPath")
         .attr("id", "clip")
         .append("rect")
-        .attr("width", width)
-        .attr("height", height);
+        .attr("width", timelineSize.width)
+        .attr("height", timelineSize.height);
 
-    var focus = svg.append("g")
+    const focus = svg.append("g")
         .attr("class", "focus")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        .attr("transform", "translate(" + timelineMargin.left + "," + timelineMargin.top + ")");
 
     d3.csv("sp500.csv", type, function (error, data) {
         if (error) throw error;
@@ -54,39 +44,24 @@ function makeTimeline() {
         x.domain(d3.extent(data, function (d) {
             return d.date;
         }));
-        y.domain([0, d3.max(data, function (d) {
-            return d.price;
-        })]);
-        x2.domain(x.domain());
-        y2.domain(y.domain());
-
-        focus.append("path")
-            .datum(data)
-            .attr("class", "area")
-            .attr("d", area);
+        x0.domain(x.domain());
 
         focus.append("g")
             .attr("class", "axis axis--x")
-            .attr("transform", "translate(0," + height + ")")
+            .attr("transform", "translate(0," + timelineSize.height + ")")
             .call(xAxis);
-
-        focus.append("g")
-            .attr("class", "axis axis--y")
-            .call(yAxis);
 
         svg.append("rect")
             .attr("class", "zoom")
-            .attr("width", width)
-            .attr("height", height)
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+            .attr("width", timelineSize.width)
+            .attr("height", timelineSize.height)
+            .attr("transform", "translate(" + timelineMargin.left + "," + timelineMargin.top + ")")
             .call(zoom);
     });
 
     function zoomed() {
-        if (d3.event.sourceEvent && d3.event.sourceEvent.type === "brush") return; // ignore zoom-by-brush
-        var t = d3.event.transform;
-        x.domain(t.rescaleX(x2).domain());
-        focus.select(".area").attr("d", area);
+        const t = d3.event.transform;
+        x.domain(t.rescaleX(x0).domain());
         focus.select(".axis--x").call(xAxis);
     }
 
